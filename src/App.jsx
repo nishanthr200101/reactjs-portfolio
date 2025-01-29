7eb7ebc3e267c9166022d9e02ecbe85f14fe7215
nishanthr200101 <nishanthr20010101@gmail.com>
2025-01-29 15:45:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
-        deploy 1
+        deploy 2
         <div className='bg-hero-pattern bg-fixed bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
